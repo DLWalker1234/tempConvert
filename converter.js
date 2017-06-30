ba76eebc0bfc159dab466d27b2bcb1f4aa6f3355
Dustin Walker <thewalkman1234@gmail.com>
2017-06-30 15:07:46 -0500

    temps

--- converter.js
+++ converter.js
@@ -1,30 +1,80 @@
+// In the HTML, have one input field where someone can enter in a temperature.
+// Create a radio button group where Celsius or Fahrenheit can be selected as the scale that the number should be converted to.
+// Create a block level element that will hold the text of the converted temperature.
+// Create a button that, when clicked, displays the converted temperature.
+// Create another button that, when clicked, clears any text in the input field.
+// Add an event handler to the input field that checks if the user pressed the enter key, and if that happens, perform the conversion.
+// If the temperature is greater than 90F/32C the color of the converted temperature should be red.
+// If the temperature is less than 32F/0C the color of the converted temperature should be blue.
+// For any other temperature, the color should be green.
+
 console.log('start');
 
-var inputTemp = document.getElementById("temp");
-var button = document.getElementById("switchTemp");
+var inputTemp = document.getElementById("inputTemp");
+var convertButton = document.getElementById("convert");
+var newTemp = document.getElementById("convertedTemp");
+// var Cinput = document.getElementById("C");
+// var Finput = document.getElementById("F");
+var clearButton = document.getElementById("clear");
 
-switchTemp.addEventListener("click", function() {
-	console.log("event",inputTemp.value);
+convertButton.addEventListener("click", function(){
+	tempPicker(inputTemp.value);
 })
 
-function toCelsius () {
-	inputTemp = (((inputTemp - 32)*5)/9);
+function tempPicker(temp) {
+	var Cinput = document.getElementById("C");
+	console.log(Cinput)
+	var Finput = document.getElementById("F");
+	console.log(Finput)
+	if (Cinput.checked === false && Finput.checked === false) {
+			alert("Choose a temp to convert to...fool")
+	} else if (Cinput.checked === true) {
+		toCelsius(temp)
+	} else if (Finput.checked === true) {
+		toFahrenheit(temp)
+	}
+}
+		
+
+function toCelsius (temp) {
+	temp = (((temp - 32)*5)/9);
+	if (temp < 0) {
+		newTemp.style.color = 'blue';
+	} else if ( temp > 0 && temp < 32) {
+		newTemp.style.color = 'green';
+	} else if ( temp > 32) {
+		newTemp.style.color = 'red';
+	}
+	newTemp.innerHTML = temp;
+
 };
 
-function toFahrenheit (inputTemp) {
-	inputTemp = ((inputTemp*9)/5)+32;
+function toFahrenheit (temp) {
+	temp = ((temp*9)/5)+32;
+	if (temp < 32) {
+		newTemp.style.color = 'blue';
+	} else if (temp > 32 && temp < 90) {
+		newTemp.style.color = 'green';
+	} else if (temp > 90) {
+		newTemp.style.color = 'red';
+	}
+	newTemp.innerHTML = temp;
 }
 
-// Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
+clearButton.addEventListener("click", function() {
+	inputTemp.value = " ";
 
-// This function should determine which conversion should
-// happen based on which radio button is selected.
-function determineConverter (clickEvent) {
-  console.log("event", clickEvent);
-}
+})
 
-// Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
+inputTemp.addEventListener("keyup", function(e) {
+	if (e.keyCode === 13) {
+		tempPicker(inputTemp.value);
+			
+		}
+	});
+
+
+
+
 
 
